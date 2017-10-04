5fd18ff256f6213de45f8aeadebdebeeb2063bf3
DIllonhill123 <dillonhill9@gmail.com>
2017-10-04 11:31:25 -0400

    Simon
    Broke

--- TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/Simon.cs
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/Simon.cs
@@ -15,28 +15,28 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
         public override void play()
         {
             Random Rando = new Random();
-            int rd1 = Rando.Next(1, 4);
-            int rd2 = Rando.Next(1, 4);
-            int rd3 = Rando.Next(1, 4);
-            int rd4 = Rando.Next(1, 4);
-            int rd5 = Rando.Next(1, 4);
+            int rd1 = Rando.Next(1, 5);
+            int rd2 = Rando.Next(1, 5);
+            int rd3 = Rando.Next(1, 5);
+            int rd4 = Rando.Next(1, 5);
+            int rd5 = Rando.Next(1, 5);
+            int counter = 0;
+            int a;
+            int b;
+            int c;
+            int d;
+            int e;
             string Ans;
-            bool isCorrect = false;
+            string Ans2;
+            string Ans3;
+            string Ans4;
+            string Ans5;
+            bool isCorrect = true;
 
             showTitle("WELCOME TO SIMON");
             writeLine("This version of simon is based off of sounds");
             writeLine("There are 4 different sounds and you should return them in the order you hear them");
             writeLine("return the correct order 5 times and you win!");
-            writeLine("Here are the 4 different sounds youll hear");
-
-            beep(100, 1);
-            wait(1);
-            beep(300, 1);
-            wait(1);
-            beep(600, 1);
-            wait(1);
-            beep(900, 1);
-            wait(1);
 
             writeLine("each sound is assigned to a number 1-4");
             writeLine("1 is"); beep(100, 1);
@@ -44,177 +44,205 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
             writeLine("3 is"); beep(600, 1);
             writeLine("4 is"); beep(900, 1);
 
-            
-            if (rd1 == 1)
+            while(isCorrect == true || counter <= 5)
             {
-                beep(100, 1);
-            }
-            if (rd1 == 2)
-            {
-                beep(300, 1);
-            }
-            if (rd1 == 3)
-            {
-                beep(600, 1);
-            }
-            if (rd1 == 4)
-            {
-                beep(900, 1);
-            }
-
-            writeLine("Enter your answer:");
-            Ans = getInput();
-            int s = Convert.ToInt32(Ans);
-
-            if (s == rd1)
-            {
-                writeLine("Correct!");
-                isCorrect = true;
-            }
-            else
-            {
-                writeLine("Wrong! You Lose!");
-            }
-
-           if(isCorrect == true)
-            {
-                if (rd2 == 1)
+                if (rd1 == 1)
                 {
                     beep(100, 1);
                 }
-                if (rd2 == 2)
+                if (rd1 == 2)
                 {
                     beep(300, 1);
                 }
-                if (rd2 == 3)
+                if (rd1 == 3)
                 {
                     beep(600, 1);
                 }
-                if (rd2 == 4)
+                if (rd1 == 4)
                 {
                     beep(900, 1);
                 }
 
                 writeLine("Enter your answer:");
                 Ans = getInput();
-                s = Convert.ToInt32(Ans);
+                a = Convert.ToInt32(Ans);
 
-                if (s == rd2)
+                if (a == rd1 && counter == 0)
                 {
                     writeLine("Correct!");
-
+                    counter++;
+                    writeOut("Round 2");
                 }
-                else
+                if(a != rd1 && counter == 0)
                 {
                     writeLine("Wrong! You Lose!");
                     isCorrect = false;
                 }
 
+                if (counter >= 1)
+                {
+                    if (rd2 == 1)
+                    {
+                        beep(100, 1);
+                    }
+                    if (rd2 == 2)
+                    {
+                        beep(300, 1);
+                    }
+                    if (rd2 == 3)
+                    {
+                        beep(600, 1);
+                    }
+                    if (rd2 == 4)
+                    {
+                        beep(900, 1);
+                    }
+
+                    writeLine("Enter your answers:");
+                    Ans = getInput();
+                    Ans2 = getInput();
+                    a = Convert.ToInt32(Ans);
+                    b = Convert.ToInt32(Ans2);
+
+                    if (b == rd2 && a == rd1 && counter == 1)
+                    {
+                        writeLine("Correct!");
+                        counter++;
+                        writeOut("Round 3");
+                    }
+                    if(b != rd2)
+                    {
+                        writeLine("Wrong! You Lose!");
+                        isCorrect = false;
+                    }
+
+                }
+                if (counter >= 2)
+                {
+                    if (rd3 == 1)
+                    {
+                        beep(100, 1);
+                    }
+                    if (rd3 == 2)
+                    {
+                        beep(300, 1);
+                    }
+                    if (rd3 == 3)
+                    {
+                        beep(600, 1);
+                    }
+                    if (rd3 == 4)
+                    {
+                        beep(900, 1);
+                    }
+
+                    writeLine("Enter your answer:");
+                    Ans = getInput();
+                    Ans2 = getInput();
+                    Ans3 = getInput();
+                    a = Convert.ToInt32(Ans);
+                    b = Convert.ToInt32(Ans2);
+                    c = Convert.ToInt32(Ans3);
+
+                    if (c == rd3 && b == rd2 && a == rd1 && counter == 3)
+                    {
+                        writeLine("Correct!");
+                        counter++;
+                    }
+                    if(c != rd3)
+                    {
+                        writeLine("Wrong! You Lose!");
+                        isCorrect = false;
+                    }
+
+                }
+                if (counter >= 3)
+                {
+                    if (rd4 == 1)
+                    {
+                        beep(100, 1);
+                    }
+                    if (rd4 == 2)
+                    {
+                        beep(300, 1);
+                    }
+                    if (rd4 == 3)
+                    {
+                        beep(600, 1);
+                    }
+                    if (rd4 == 4)
+                    {
+                        beep(900, 1);
+                    }
+
+                    writeLine("Enter your answer:");
+                    Ans = getInput();
+                    Ans2 = getInput();
+                    Ans3 = getInput();
+                    Ans4 = getInput();
+                    a = Convert.ToInt32(Ans);
+                    b = Convert.ToInt32(Ans2);
+                    c = Convert.ToInt32(Ans3);
+                    d = Convert.ToInt32(Ans4);
+
+                    if (d == rd4 && c ==rd3 && b == rd2 && a == rd1 && counter == 4)
+                    {
+                        writeLine("Correct!");
+                        counter++;
+                    }
+                    if(d != rd4)
+                    {
+                        writeLine("Wrong! You Lose!");
+                        isCorrect = false;
+                    }
+
+                }
+                if (counter >= 4)
+                {
+                    if (rd5 == 1)
+                    {
+                        beep(100, 1);
+                    }
+                    if (rd5 == 2)
+                    {
+                        beep(300, 1);
+                    }
+                    if (rd5 == 3)
+                    {
+                        beep(600, 1);
+                    }
+                    if (rd5 == 4)
+                    {
+                        beep(900, 1);
+                    }
+
+                    writeLine("Enter your answer:");
+                    Ans = getInput();
+                    Ans2 = getInput();
+                    Ans3 = getInput();
+                    Ans4 = getInput();
+                    Ans5 = getInput();
+                    a = Convert.ToInt32(Ans);
+                    b = Convert.ToInt32(Ans2);
+                    c = Convert.ToInt32(Ans3);
+                    d = Convert.ToInt32(Ans4);
+                    e = Convert.ToInt32(Ans5);
+
+                    if (e == rd5 && d == rd4 && c == rd3 && b == rd2 && a == rd1 && counter == 5)
+                    {
+                        writeLine("Correct!");
+                        counter++;
+                        writeOut("YOU WIN!!");
+                    }
+                    if(e != rd5)
+                    {
+                        writeLine("Wrong! You Lose!");
+                        isCorrect = false;
+                    }
+
+                }
             }
-            if (isCorrect == true)
-            {
-                if (rd3 == 1)
-                {
-                    beep(100, 1);
-                }
-                if (rd3 == 2)
-                {
-                    beep(300, 1);
-                }
-                if (rd3 == 3)
-                {
-                    beep(600, 1);
-                }
-                if (rd3 == 4)
-                {
-                    beep(900, 1);
-                }
 
-                writeLine("Enter your answer:");
-                Ans = getInput();
-                s = Convert.ToInt32(Ans);
-
-                if (s == rd3)
-                {
-                    writeLine("Correct!");
-
-                }
-                else
-                {
-                    writeLine("Wrong! You Lose!");
-                    isCorrect = false;
-                }
-
-            }
-            if (isCorrect == true)
-            {
-                if (rd4 == 1)
-                {
-                    beep(100, 1);
-                }
-                if (rd4 == 2)
-                {
-                    beep(300, 1);
-                }
-                if (rd4 == 3)
-                {
-                    beep(600, 1);
-                }
-                if (rd4 == 4)
-                {
-                    beep(900, 1);
-                }
-
-                writeLine("Enter your answer:");
-                Ans = getInput();
-                s = Convert.ToInt32(Ans);
-
-                if (s == rd4)
-                {
-                    writeLine("Correct!");
-
-                }
-                else
-                {
-                    writeLine("Wrong! You Lose!");
-                    isCorrect = false;
-                }
-
-            }
-            if (isCorrect == true)
-            {
-                if (rd5 == 1)
-                {
-                    beep(100, 1);
-                }
-                if (rd5 == 2)
-                {
-                    beep(300, 1);
-                }
-                if (rd5 == 3)
-                {
-                    beep(600, 1);
-                }
-                if (rd5 == 4)
-                {
-                    beep(900, 1);
-                }
-
-                writeLine("Enter your answer:");
-                Ans = getInput();
-                s = Convert.ToInt32(Ans);
-
-                if (s == rd5)
-                {
-                    writeLine("Correct!");
-                }
-                else
-                {
-                    writeLine("Wrong! You Lose!");
-                    isCorrect = false;
-                }
-
-            }
 
 
         }
